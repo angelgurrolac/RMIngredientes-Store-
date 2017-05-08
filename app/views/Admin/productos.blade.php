419a66c6554e1f3641329cdac912689bc0e80b47
Zayra Chávez <zayrachavez@hotmail.com>
2017-05-07 23:06:08 -0500

    Productos Tienda

--- app/views/Admin/productos.blade.php
+++ app/views/Admin/productos.blade.php
@@ -119,6 +119,12 @@
             <!-- /.navbar-header -->
 
             <ul class="nav navbar-top-links navbar-right bajar-barra">
+              <!-- <li class="dropdown">
+                <a href="">Hola</a>
+              </li>
+              <li class="dropdown">
+                <a href="Hola">Adiós</a>
+              </li> -->
               <li class="dropdown">
                 <a class="dropdown-toggle estilo-user" data-toggle="dropdown" href="#">
                   <i class="glyphicon glyphicon-user"></i> {{ Session::get('nombre')}} <i class="fa fa-caret-down"></i>
